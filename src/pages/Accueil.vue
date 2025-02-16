2307f4b38feed2b039ba779eb14795fd8292bf89
Thomas <thomas.neang@khresterion.com>
2025-02-16 14:26:04 +0100

    adding github link

--- src/pages/Accueil.vue
+++ src/pages/Accueil.vue
@@ -5,6 +5,7 @@
         <p>Ce projet est en cours pour une démonstration de mes compétences fullstack. Son style est toujours en cours</p>
         <p>Pour le moment il y a un affichage dynamique de fausse valeur boursière. Le but est de mettre en place l'infrastructure pour déployer mes applications. 
           Je trouverai de vrai projet à montrer ici plus tard</p>
+          <p>Mon <a href="https://github.com/thneang" target="_blank">github</a> est aussi disponible.</p>
         <ul class="ma-4">
           <li><strong>Back-end :</strong> Spring Boot avec WebSockets</li>
           <li><strong>Front-end :</strong> Vue.js avec Vuetify</li>
