feb31b930ef70e9506743874f64d6df79d7b0db2
Thomas <thomas.neang@khresterion.com>
2025-02-16 18:08:01 +0100

    update description

--- src/pages/Accueil.vue
+++ src/pages/Accueil.vue
@@ -4,13 +4,15 @@
         <h1>Bienvenue</h1>
         <p>Ce projet est en cours pour une démonstration de mes compétences fullstack. Son style est toujours en développement.</p>
         <p>Pour le moment, il y a un affichage dynamique de fausses valeurs boursières. </p>
-        <p>Le but est de mettre en place l'infrastructure pour déployer mes applications. 
-            Je trouverai de vrais projets à montrer ici plus tard.</p>
+        <p>Les applications en elles-mêmes sont plutôt simples mais le but était de mettre en place mon infrastructure rapidement en 2 jours. </p>
           <p>Mon <a href="https://github.com/thneang" target="_blank">github</a> est aussi disponible.</p>
         <ul class="ma-4">
           <li><strong>Back-end :</strong> Spring Boot avec WebSockets</li>
           <li><strong>Front-end :</strong> Vue.js avec Vuetify</li>
           <li><strong>Demo :</strong> Simulation de valeurs boursières dynamiques</li>
+          <li><strong>Nginx</strong> Pour avoir un reverse proxy et router les requêtes</li>
+          <li><strong>Ansible</strong> Pour configurer le server</li>
+          <li><strong>Jenkins</strong> Pour build les applications. En cours de configuration.</li>
         </ul>
       </v-card>
     </v-container>
