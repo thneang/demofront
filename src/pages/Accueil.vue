ecde4e38f04f2716687f470be268e3c2f144ac2c
Thomas <thomas.neang@khresterion.com>
2025-02-16 17:11:51 +0100

    update description

--- src/pages/Accueil.vue
+++ src/pages/Accueil.vue
@@ -2,9 +2,10 @@
     <v-container>
       <v-card class="pa-4">
         <h1>Bienvenue</h1>
-        <p>Ce projet est en cours pour une démonstration de mes compétences fullstack. Son style est toujours en cours</p>
-        <p>Pour le moment il y a un affichage dynamique de fausse valeur boursière. Le but est de mettre en place l'infrastructure pour déployer mes applications. 
-          Je trouverai de vrai projet à montrer ici plus tard</p>
+        <p>Ce projet est en cours pour une démonstration de mes compétences fullstack. Son style est toujours en développement.</p>
+        <p>Pour le moment, il y a un affichage dynamique de fausses valeurs boursières. </p>
+        <p>Le but est de mettre en place l'infrastructure pour déployer mes applications. 
+            Je trouverai de vrais projets à montrer ici plus tard.</p>
           <p>Mon <a href="https://github.com/thneang" target="_blank">github</a> est aussi disponible.</p>
         <ul class="ma-4">
           <li><strong>Back-end :</strong> Spring Boot avec WebSockets</li>
